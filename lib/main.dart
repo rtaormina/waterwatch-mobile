4bba0bec866930d591473965798815d0a0424904
Pieter van den Haspel <P.B.A.vandenhaspel@student.tudelft.nl>
2025-06-03 10:56:06 +0200

    skeleton mobile app

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:waterwatch/screens/home_screen.dart';
+
+void main() async {
+  runApp(
+    const MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
