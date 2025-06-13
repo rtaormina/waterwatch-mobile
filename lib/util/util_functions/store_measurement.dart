eac6eb83b1e02f7aff384291af8380a89a8c35c6
Pieter van den Haspel <P.B.A.vandenhaspel@student.tudelft.nl>
2025-06-13 11:42:22 +0200

    restructure

--- lib/util/util_functions/store_measurement.dart
+++ lib/util/util_functions/store_measurement.dart
@@ -1,3 +1,47 @@
-Future<void> storeMeasurement() {
-  
+import 'dart:convert';
+
+import 'package:intl/intl.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:waterwatch/util/measurement_state.dart';
+import 'package:waterwatch/util/metric_objects/temperature_object.dart';
+
+const _kMeasurementsKey = 'stored_measurements';
+
+Future<void> storeMeasurement(
+  String apiUrl, // retained in signature but not stored locally
+  LatLng? location,
+  String waterSource,
+  TemperatureObject metricTemperatureObject,
+) async {
+  final prefs = await SharedPreferences.getInstance();
+
+  // 1) Gather timestamps and format
+  final now = DateTime.now();
+  final today = DateFormat('yyyy-MM-dd').format(now);
+  final time = DateFormat('HH:mm:ss').format(now);
+
+  // 2) Build the payload map
+  final measurement = {
+    'timestamp': now.toUtc().toIso8601String(),
+    'local_date': today,
+    'local_time': time,
+    'location': {
+      'type': 'Point',
+      'coordinates': [location!.longitude, location.latitude],
+    },
+    'water_source': waterSource,
+    'temperature': {
+      'value': metricTemperatureObject.temperature,
+      'sensor': metricTemperatureObject.sensorType,
+      'time_waited': formatDurationToMinSec(metricTemperatureObject.duration),
+    },
+  };
+
+  // 3) Load existing list (or start fresh), append, and save
+  final rawList = prefs.getStringList(_kMeasurementsKey) ?? <String>[];
+  rawList.add(jsonEncode(measurement));
+  await prefs.setStringList(_kMeasurementsKey, rawList);
 }
+
+
--- lib/util/util_functions/upload_measurement.dart
+++ lib/util/util_functions/upload_measurement.dart
@@ -1,18 +1,12 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'package:intl/intl.dart';
-import 'package:latlong2/latlong.dart';
 import 'package:waterwatch/util/measurement_state.dart';
-import 'package:waterwatch/util/metric_objects/temperature_object.dart';
 
 Future<void> uploadMeasurement(
-  String apiUrl,
-  LatLng? location,
-  String waterSource,
-  TemperatureObject metricTemperatureObject,
+  Map<String, dynamic> measurement,
 ) async {
-  String url = "$apiUrl/api/measurements/";
+  String url = "https://waterwatch.tudelft.nl/api/measurements/";
   final uri = Uri.parse(url);
 
   String token = await getCSRFToken();
@@ -26,31 +20,10 @@ Future<void> uploadMeasurement(
     'Cookie': 'csrftoken=$token',
   };
 
-  final now = DateTime.now();
-  final today = DateFormat('yyyy-MM-dd').format(now);
-  final time = DateFormat('HH:mm:ss').format(now);
+  final body = jsonEncode(measurement);
 
-  // Encode your payload as JSON
-  final body = jsonEncode({
-    "timestamp": DateTime.now().toUtc().toIso8601String(),
-    "local_date": today,
-    "local_time": time,
-    "location": {
-      "type": "Point",
-      "coordinates": [location!.longitude, location.latitude]
-    },
-    "water_source": waterSource,
-    "temperature": {
-      "value": metricTemperatureObject.temperature,
-      "sensor": metricTemperatureObject.sensorType,
-      "time_waited": formatDurationToMinSec(metricTemperatureObject.duration)
-    }
-  });
-
-  // Send the POST
   final response = await http.post(uri, headers: headers, body: body);
 
-  // Check status code
   if (response.statusCode != 200 && response.statusCode != 201) {
     throw http.ClientException(
       'Failed POST (${response.statusCode}): ${response.body}',
